ca48e2e8eea68e946978bae7a6c8eec070bce02f
foukou19 <ferial.oukoukas@decathlon.com>
2026-07-24 11:44:38 +0200

    feat(entity-dynamic-mapping): update entity relation
    Signed-off-by: foukou19 <ferial.oukoukas@decathlon.com>

--- src/test/java/com/decathlon/idp_core/infrastructure/adapters/api/handler/ApiExceptionHandlerTest.java
+++ src/test/java/com/decathlon/idp_core/infrastructure/adapters/api/handler/ApiExceptionHandlerTest.java
@@ -194,10 +194,10 @@ class ApiExceptionHandlerTest {
             .handlePropertyNameNotFoundEntityTemplatePropertiesException(exception);
 
         assertNotNull(response);
-        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(HttpStatus.UNPROCESSABLE_CONTENT, response.getStatusCode());
         ErrorResponse body = response.getBody();
         assertNotNull(body);
-        assertEquals(HttpStatus.BAD_REQUEST.name(), body.getError());
+        assertEquals(HttpStatus.UNPROCESSABLE_CONTENT.name(), body.getError());
         assertEquals(details, body.getErrorDescription());
       }
 
