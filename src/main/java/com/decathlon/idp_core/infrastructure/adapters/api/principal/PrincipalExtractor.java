f61294c0d6b85e14f505cd0da0782a2fb5bc4907
rvando12 <renny.vandomber@decathlon.com>
2026-07-31 14:23:34 +0200

    feat(core): human check fix

--- src/main/java/com/decathlon/idp_core/infrastructure/adapters/api/principal/PrincipalExtractor.java
+++ src/main/java/com/decathlon/idp_core/infrastructure/adapters/api/principal/PrincipalExtractor.java
@@ -3,7 +3,6 @@ package com.decathlon.idp_core.infrastructure.adapters.api.principal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.security.core.Authentication;
