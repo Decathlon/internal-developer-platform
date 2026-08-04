5871364ebe70fd66d5b6290edfa9a9d2d8c10ef2
Andrés Brand <andres.brand.partner@decathlon.com>
2026-08-04 20:18:18 +0200

    feat(core): init camel ingestion route

--- src/main/java/com/decathlon/idp_core/infrastructure/adapters/ingestion/processor/SecurityProcessor.java
+++ src/main/java/com/decathlon/idp_core/infrastructure/adapters/ingestion/processor/SecurityProcessor.java
@@ -1,13 +1,19 @@
 package com.decathlon.idp_core.infrastructure.adapters.ingestion.processor;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Component;
+
+import com.decathlon.idp_core.domain.model.inbound_connectors.webhook.WebhookConnector;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
 public class SecurityProcessor {
-  public boolean validate() {
+  public boolean validate(Map<String, Object> headers, WebhookConnector webhookConnector) {
+    // Implement security validation logic here
+    log.info("WebhookSecurity headers: {} strategy: {}", headers, webhookConnector.security());
     return true;
   }
 }
