ed8a89f3a26e4076f755eaae671443e9b1bc3e63
foukou19 <ferial.oukoukas@decathlon.com>
2026-08-13 16:05:19 +0200

    feat(core): update SecurityProcessor
    Signed-off-by: foukou19 <ferial.oukoukas@decathlon.com>

--- src/main/java/com/decathlon/idp_core/infrastructure/adapters/ingestion/processor/SecurityProcessor.java
+++ src/main/java/com/decathlon/idp_core/infrastructure/adapters/ingestion/processor/SecurityProcessor.java
@@ -1,18 +1,13 @@
 package com.decathlon.idp_core.infrastructure.adapters.ingestion.processor;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Component;
-
-import com.decathlon.idp_core.domain.model.inbound_connectors.webhook.WebhookConnector;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
 public class SecurityProcessor {
-  public boolean validate(Map<String, Object> headers, WebhookConnector webhookConnector) {
-    // Implement security validation logic here
+  public boolean validate() {
     return true;
   }
 }
